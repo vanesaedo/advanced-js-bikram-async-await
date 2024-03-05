8dd52d59b670115b2600059c5d79d05806b59b8d
Vanesa Edo <vanesa.en@gmail.com>
2024-03-05 14:03:24 +0100

    Solution Ex.1 added.)

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 //DESARROLLA AQUI TUS SOLUCIONES
 
-async function getImageAndName (pokemon){
+async function getRandomPokemon (){
+    try{
+    let i = Math.floor(Math.random()*150);
+   
+    let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${i}`)
+    let data = await response.json();
+    
+   return data;
+   
+    }
+    catch(error){
+    console.log('Datos no recibidos');
+    }
+}
+
+
+ async function getImageAndName (pokemon){
 
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     let data = await response.json();
@@ -8,4 +24,9 @@ async function getImageAndName (pokemon){
     let img = data.sprites.front_default;
     return {name, img}
     
-}
+} 
+
+  
+   
+    
+ 
